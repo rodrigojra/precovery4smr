fac3d9fdebd0602173b62d80606d9c89e52b06f4
Rodrigo Antunes <rodrigojra@gmail.com>
2020-06-18 22:52:48 -0300

    Fixed junit test

--- test/bftsmart/parallelism/late/graph/LockFreeGraphTest.java
+++ test/bftsmart/parallelism/late/graph/LockFreeGraphTest.java
@@ -15,8 +15,8 @@ public class LockFreeGraphTest {
 
 	@Before
 	public void setUp() throws Exception {
-		limit = 100;
-		initialData = 10;
+		limit = 10;
+		initialData = 5;
 		cos = new LockFreeGraph(limit + initialData);
 		CommandsInfo cmdInfo = null;
 
@@ -35,7 +35,7 @@ public class LockFreeGraphTest {
 	@Test
 	public void testInsert() {
 		CommandsInfo cmdInfo = null;
-		
+
 		for (int i = initialData; i < limit; i++) {
 			try {
 				cmdInfo = new CommandsInfo(Integer.valueOf(i));
@@ -51,33 +51,34 @@ public class LockFreeGraphTest {
 	@Test
 	public void testGet() {
 		try {
-			
-            Object node = cos.get();
-            CommandsInfo cmdInfo = (CommandsInfo) ((DependencyGraph.vNode) node).getData();
-			System.out.println("LockFreeGraphTest: testGet: cosGetResult: " +  cmdInfo.getID());
+
+			Object node = cos.get();
+			CommandsInfo cmdInfo = (CommandsInfo) ((DependencyGraph.vNode) node).getData();
+			System.out.println("LockFreeGraphTest: testGet: cosGetResult: " + cmdInfo.getID());
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail("LockFreeGraphTest: testGet: fail");
 		}
-	}	
-	
+	}
+
 	@Test
 	public void testRemove() {
-		
-		try {
-			
-            Object node = cos.get();
-            CommandsInfo cosGetResult = (CommandsInfo) ((DependencyGraph.vNode) node).getData();			
 
-			System.out.println("LockFreeGraphTest: testRemove: before - cosGetResult: " +  cosGetResult.getID());
+		try {
+
+			Object node = cos.get();
+			CommandsInfo cosGetResult = (CommandsInfo) ((DependencyGraph.vNode) node).getData();
+
+			System.out.println("LockFreeGraphTest: testRemove: before - cosGetResult: " + cosGetResult.getID());
 			cos.remove(node);
+			node = cos.get();
 			cosGetResult = (CommandsInfo) ((DependencyGraph.vNode) node).getData();
-			System.out.println("LockFreeGraphTest: testRemove: after - cosGetResult: " +  cosGetResult.getID());
+			System.out.println("LockFreeGraphTest: testRemove: after - cosGetResult: " + cosGetResult.getID());
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail("LockFreeGraphTest: testGet: fail");
-		}		
+		}
 	}
 }
