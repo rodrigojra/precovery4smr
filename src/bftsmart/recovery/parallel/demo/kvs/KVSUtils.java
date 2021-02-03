0004b171b0af2e357915f559d593388e45edd1ab
Rodrigo Antunes <rodrigojra@gmail.com>
2021-02-03 09:51:24 -0300

    Revert "fixed issues to identify generated operation"
    This reverts commit d1b08ab0e3ab07b607981283ee8abf38b3dae326.

--- src/bftsmart/recovery/parallel/demo/kvs/KVSUtils.java
+++ src/bftsmart/recovery/parallel/demo/kvs/KVSUtils.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Rodrigo Antunes
@@ -19,108 +18,43 @@ public class KVSUtils {
 	public static byte[] generateRandomOperation(Random random, int maxKey, float sparseness, float conflict) {
 		KVSRequestType requestType;
 		byte[] byteArrayToReturn = null;
-		requestType = generateOperationType(random);
-
-		int key, mid = maxKey / 2;
-		do {
-			key = (int) Math.round(random.nextGaussian() * (mid * sparseness) + mid);
-		} while (key < 0 || key >= maxKey);
-
-		try {
-			ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-			ObjectOutput objOut = new ObjectOutputStream(byteOut);
-
-			objOut.writeObject(requestType);
-			objOut.writeObject(key);
-
-			if (requestType == KVSRequestType.PUT) {
-				objOut.writeObject(random.nextInt());
-			}
-
-			objOut.flush();
-			byteOut.flush();
-
-			byteArrayToReturn = byteOut.toByteArray();
-		} catch (IOException e) {
-			System.out.println("Exception putting value into map: " + e.getMessage());
-		}
-
-		return byteArrayToReturn;
-	}
-
-	/**
-	 * @param random
-	 * @return
-	 */
-	private static KVSRequestType generateOperationType(Random random) {
-		KVSRequestType requestType;
-		int writableOP = random.nextInt(2);
-		int randomOp = random.nextInt(2);
-		if (writableOP == 0) {
+		int randomOp = random.nextInt(1);
+		if (random.nextFloat() >= conflict) {
 			if (randomOp == 0) {
-				requestType = KVSRequestType.GET;
+				requestType = KVSRequestType.GET;	
 			} else {
 				requestType = KVSRequestType.SIZE;
 			}
 		} else {
 			if (randomOp == 0) {
-				requestType = KVSRequestType.PUT;
+				requestType = KVSRequestType.PUT;	
 			} else {
 				requestType = KVSRequestType.REMOVE;
 			}
 		}
-		System.out.println(">> requestType " + requestType);
-		return requestType;
-	}
 
-	/**
-	 * @param reply
-	 */
-	public static void printBytes(byte[] reply) {
-		System.out.print("\t[ ");
-		for (byte b : reply) {
-			System.out.print(", " + b);
-		}
-		System.out.println(" ]");
-	}
-	
-	public static void printOperationData(KVSRequestType type, int key, int value) {
-		String operation = "";
-		switch (type) {
-		case PUT:
-			operation = "PUT";
-			break;
-		case GET:
-			operation = "GET";
-			break;
-		case REMOVE:
-			operation = "REMOVE";
-			break;
-		case KEYSET:
-			operation = "KEYSET";
-			break;
-		default:
-			operation = "EMPTY";
-			break;
-		}
+		int key, mid = maxKey / 2;
+		do {
+			key = (int) Math.round(random.nextGaussian() * (mid * sparseness) + mid);
+		} while (key < 0 || key >= maxKey);
+		
+		try {
+			ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+			ObjectOutput objOut = new ObjectOutputStream(byteOut);
+			
+			objOut.writeObject(requestType);
+			objOut.writeObject(key);
+			objOut.writeObject(random.nextInt());
+			
+			objOut.flush();
+			byteOut.flush();
+			
+			byteArrayToReturn = byteOut.toByteArray();
 
-		if (value == -1) {
-			System.out.println("received [" + operation + "] key [" + key + "]");
-		} else {
-			System.out.println("received [" + operation + "] key [" + key + "] value[" + value + "]");
-		}
-	}	
+		} catch (IOException e) {
+			System.out.println("Exception putting value into map: " + e.getMessage());
+		}			
 
-	public static void main(String[] args) {
-		ThreadLocalRandom random = ThreadLocalRandom.current();
-		//int maxKey = 1000;
-		//float sparseness = 10.0f;
-		//float conflict = 1.0f;
-		// generateRandomOperation(random, maxKey, sparseness, conflict);
-
-		for (int i = 0; i < 10; i++) {
-			generateOperationType(random);
-		}
-
+		return byteArrayToReturn;
 	}
 }
